bcce5f1d4c1ea3f34680844f91ef9d5fb3d2890d
Dustin Neighly <neighlyd@gmail.com>
2016-08-11 16:31:49 -0700

    Added security settings for production site.

--- knights_of_climbalot/settings/production.py
+++ knights_of_climbalot/settings/production.py
@@ -6,3 +6,19 @@ DEBUG = False
 
 # Set allowed hosts to url.
 ALLOWED_HOSTS = ['.knightsofclimbalot.com', '.knightsofclimbalot.com.']
+
+# Security settings
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_BROWSER_XSS_FILTER = True
+CSRF_COOKIE_HTTPONLY = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_HSTS_SECONDS = 3
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_PROXY_SSL_HEADER = True
+
+
+# Prevents a frame from serving content from another frame. Suggested to set as false for production, but may change with content generation. We will see.
+X_FRAME_OPTIONS = 'DENY'
